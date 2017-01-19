70d7156badef41ddc55f426af3b7bab2b05f9b11
Milos Mosovsky <milos.mosovsky@toptal.com>
2017-01-19 01:48:04 +0100

    fixup! Initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 var rpio = require('rpio');
+rpio.init({gpiomem: false});
 
-rpio.open(15, rpio.OUTPUT, rpio.LOW);
+var RED_PORT = 15;
+var GREEN_PORT = 16;
+var GREEN_PORT = 16;
+
+rpio.open(RED_PORT, rpio.OUTPUT, rpio.LOW);
 
 for (var i = 0; i < 5; i++) {
     /* On for 1 second */
-    rpio.write(15, rpio.HIGH);
+    rpio.write(RED_PORT, rpio.HIGH);
+    rpio.write(GREEN_PORT, rpio.HIGH);
     rpio.sleep(1);
 
     /* Off for half a second (500ms) */
-    rpio.write(15, rpio.LOW);
+    rpio.write(RED_PORT, rpio.LOW);
+    rpio.write(GREEN_PORT, rpio.LOW);
     rpio.msleep(500);
 }
